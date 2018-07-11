5cc533aaec52b1e6524a8be1b00b7ee89e43ea50
abejide femi adeola <bjhaid@localhost.localdomain>
2018-07-11 11:59:56 +0100

    changes made to index.js

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const  cheerio = require('cheerio');
-const jsonframe = require('jsonframe-cheerio');
 const axios = require('axios');
 
-axios.get('https://www.oanda.com/currency/live-exchange-rates/#Majors')
-    .then((response) => {
-        if(response.status === 200) {
-        const html = response.data;
-            const $ = cheerio.load(html); 
-            let currency = [];
-            $('.inline_rates_container').each(function(i, elem) {
-                currency[i] = {
-                    title: $(this).find('.bigger').text().trim().split(' ')
-                } 
-            })  
-            console.log(currency);
-            
-    }
-    }, (error) => console.log(err) );
+let base_url = 'http://www.bca.co.id/id/Individu/Sarana/Kurs-dan-Suku-Bunga/Kurs-dan-Kalkulator';
+
+axios.get(base_url).then( (response) => {
+    let $ = cheerio.load(response.data);
+    let kurs = [];
+    $('tr', '.text-right').each( (i, elm) => {
+        kurs.push( {
+        currency: $(elm).children().first().text(),
+        erate: {
+            sell: $(elm).children().eq(1).first().text(),
+            buy: $(elm).children().eq(2).first().text()
+        },
+        tt: {
+            sell: $(elm).children().eq(3).first().text(),
+            buy: $(elm).children().eq(4).first().text()
+        },
+        notes: {
+            sell: $(elm).children().eq(5).first().text(),
+            buy: $(elm).children().eq(6).first().text()
+        }
+        });
+    });
+    return(kurs);
+    })
+    .then ( (kurs) => {
+    console.log(kurs);
+});
